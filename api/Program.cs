29d607af94fbe2880cfd48be2129c1b0a0c5b151
ShiranaiHito <aimanafiqbinesam@gmail.com>
2025-04-30 07:14:40 +0800

    enable static files for development

--- api/Program.cs
+++ api/Program.cs
@@ -103,6 +103,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseStaticFiles();
     app.UseExceptionHandler("/error-development");
 }
 else
