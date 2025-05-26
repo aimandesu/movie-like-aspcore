41f37f577721e766f672852545c741049cf9a841
aimandesu <aimanafiqbinesam@gmail.com>
2025-05-27 00:44:56 +0800

    add wwroot volume for api docker compose, change port expose

--- api/Data/ApplicationDbContext.cs
+++ api/Data/ApplicationDbContext.cs
@@ -11,24 +11,24 @@ namespace api.Data
 {
     public class ApplicationDbContext : IdentityDbContext<User>
     {
-        public ApplicationDbContext(DbContextOptions dbContextOptions) 
+        public ApplicationDbContext(DbContextOptions dbContextOptions)
         : base(dbContextOptions)
         {
-            
+
         }
 
-        public DbSet<User> Users {get; set;}
-        public DbSet<Wishlist> Wishlists {get; set;}
-        public DbSet<History> Histories {get; set;}
-        public DbSet<Impression> Impressions {get; set;}
-        public DbSet<Comment> Comments {get; set;}
-        public DbSet<Series> Series {get; set;}
-        public DbSet<SeriesCategory> SeriesCategories {get; set;}
-        public DbSet<TagCategory> TagCategories {get; set;}
-        public DbSet<Category> Categories {get; set;}
-        public DbSet<Tag> Tags {get; set;}
-        public DbSet<Episode> Episodes {get; set;}
-        public DbSet<Video> Videos {get; set;}
+        public override DbSet<User> Users { get; set; }
+        public DbSet<Wishlist> Wishlists { get; set; }
+        public DbSet<History> Histories { get; set; }
+        public DbSet<Impression> Impressions { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Series> Series { get; set; }
+        public DbSet<SeriesCategory> SeriesCategories { get; set; }
+        public DbSet<TagCategory> TagCategories { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Episode> Episodes { get; set; }
+        public DbSet<Video> Videos { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -38,15 +38,15 @@ namespace api.Data
             // .HasOne(e => e.Series)
             // .WithMany(e => e.Episodes)
             // .HasForeignKey(e => e.SeriesId);
-       
+
             // modelBuilder.Entity<Episode>()
             // .HasOne(e => e.Video)
             // .WithOne(e => e.Episode)
             // .HasForeignKey<Episode>(e => e.VideoId);
 
-             modelBuilder.Entity<Series>()
-            .Property(s => s.SeriesFormat)
-            .HasConversion<string>();
+            modelBuilder.Entity<Series>()
+           .Property(s => s.SeriesFormat)
+           .HasConversion<string>();
 
             // modelBuilder.Entity<SeriesCategory>()
             // .HasOne(sc => sc.Series)
@@ -75,6 +75,6 @@ namespace api.Data
             };
             modelBuilder.Entity<IdentityRole>().HasData(roles);
         }
-        
+
     }
 }
